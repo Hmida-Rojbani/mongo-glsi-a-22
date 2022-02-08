94f74622e7f89164de36ab488afb6689e2cfbedd
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-02-08 12:10:55 +0100

    course model

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 require('./db/connect')
+const express = require('express');
+const appDebug = require('debug')('app:debug');
+const {Course} = require('./models/course');
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+
+let course = new Course({
+    title : 'Angular',
+    author : ' S. Saoudi',
+    tags : ['front', 'ts'],
+    isPublished : 'false'
+});
+
+//console.log( course.save());
+
+app.listen(port, () => appDebug(`Server on ${port}`));
