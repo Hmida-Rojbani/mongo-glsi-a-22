fb80f2b16829a52a8dff708302fede3511abdf00
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-02-15 11:53:32 +0100

    some operators

--- routers/courses.js
+++ routers/courses.js
@@ -17,8 +17,51 @@ router.post('',async (req,res) =>{
     res.send(course);
 });
 
+// get all documents from courses with given title
+router.get('/title/:t',async (req,res) =>{
+    let courses = await Course.find({title : req.params.t});
+    res.send(courses);
+});
 
+// get all documents from courses with titles start with  
+router.get('/title/start/:prefixe',async (req,res) =>{
+    let courses = await Course.find({title : new RegExp('^'+req.params.prefixe,'i')},'title author price -_id')
+                                .sort({author : 1, price : 1})
+                                .skip(1)
+                                .limit(2);
+    res.send(courses);
+});
 
+// get all documents from courses with price > val 
+router.get('/price/over/:p',async (req,res) =>{
+    let courses = await Course.find({price : { $gte : req.params.p }})
+                                
+    res.send(courses);
+});
 
+// get all documents from courses with price < val 
+//operators 
+// $eq, $neq
+// $lt, $lte, $gt, $gte
+// $in, $nin
+
+router.get('/price/under/:p',async (req,res) =>{
+    let courses = await Course.find({price : { $lte : req.params.p }})
+                                
+    res.send(courses);
+});
+
+// router.get('/price/in/:minp/:maxp',async (req,res) =>{
+//     let courses = await Course.find({price : { $lte : req.params.maxp, $gte : req.params.minp }})
+                                
+//     res.send(courses);
+// });
+
+router.get('/price/in/:minp/:maxp',async (req,res) =>{
+    let courses = await Course.find({price : { $lte : req.params.maxp }})
+                                .and({price : { $gte : req.params.minp }})
+                                
+    res.send(courses);
+});
 
 module.exports=router;
